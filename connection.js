f45b27ffb7ff5112fc844f0a020f5ec4224fedb7
Ryan Evans <rdevans87@gmail.com>
2021-05-25 05:38:02 -0400

    created seed.sql to pre populate data

--- connection.js
+++ connection.js
@@ -19,3 +19,4 @@ const connection = mysql.createConnection(
 connection.connect();
 
 
+
---
+++ seed.sql
@@ -0,0 +1,13 @@
+USE employeeTracker_db;
+
+INSERT INTO department 
+
+VALUES
+('Sales'),
+('Support'),
+('Marketing'),
+('Development');
+
+
+INSERT INTO
+INSERT INTO
