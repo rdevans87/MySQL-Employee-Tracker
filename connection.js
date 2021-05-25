907fb0eb2a1e022ae423d5cfc01f12586db63420
Ryan Evans <rdevans87@gmail.com>
2021-05-25 05:20:28 -0400

    setting up connection with mysql added .env file to store environmental variables

--- connection.js
+++ connection.js
@@ -1,2 +1,21 @@
-// **C**ontent **M**anagement **S**ystem.
+const mysql = require('mysql'),
+
+// const inquirer = require('inquirer')
+
+const dotenv = require('dotenv').config();
+
+const connection = mysql.createConnection(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+   { 
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+   }
+
+);
+
+connection.connect();
+
 
