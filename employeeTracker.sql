36b2d81d9dd45916d5c947f34ef5dec8cfa1c67f
Ryan Evans <rdevans87@gmail.com>
2021-05-25 03:54:47 -0400

    inputing table data from readme to create tables in schema

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -6,16 +6,31 @@ CREATE DATABASE employeeTracker_db;
 USE employeerTracker_db;
 
 -- DATABASE SCHEMA CONTAINING THREE TABLES --
-CREATE TABLE employee (
-
-);
 
 CREATE TABLE department (
-
+    id INT PRIMARY KEY AUTOINCREMENT
+    name VARCHAR(30) NOT NULL
 
 );
 
 CREATE TABLE role(
+    id INT PRIMARY KEY AUTOINCREMENT
+    title VARCHAR(30) to hold role title
+    salary DECIMAL to hold role salary
+    department_id to hold reference to department role belongs to 
+
+
+);
+
+CREATE TABLE employee (
+
+  id INT PRIMARY KEY AUTOINCREMENT
+  first_name VARCHAR(30) to hold employee first name
+  last_name VARCHAR(30) to hold employee last name
+  role_id INT to hold reference to role employee has
+  manager_id INT to hold reference to another employee 
+  that manages the employee being Created. This field may be 
+  null if the employee has no manager
 
 
 );
