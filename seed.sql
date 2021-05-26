6ccea11caa24572e134f08d10887007d8e262a52
Ryan Evans <rdevans87@gmail.com>
2021-05-26 05:15:40 -0400

    added values to seeds employee title salary department

--- seed.sql
+++ seed.sql
@@ -13,7 +13,14 @@ INSERT INTO role
 title, salary, department_id
 
 VALUES
-('sales qa')
+('Sales Manager', 60000, 1)
+('Sales Associate', 40000, 1)
+('Support Manager', 65000, 2)
+('Support Associate', 6500, 2)
+('Marketing Manager', 70000, 3)
+('Marketing Associate', 50000, 3)
+('Development Manager', 100000, 4)
+('Development Associate', 80000, 4 )
 
 
 
