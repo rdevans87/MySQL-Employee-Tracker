a5ae29a60d36923ed59bf1bd6cdcbdf61146d667
Ryan Evans <rdevans87@gmail.com>
2021-05-26 05:31:01 -0400

    update to seed js add employee data to 3rd table

--- seed.sql
+++ seed.sql
@@ -13,15 +13,26 @@ INSERT INTO role
 title, salary, department_id
 
 VALUES
-('Sales Manager', 60000, 1)
-('Sales Associate', 40000, 1)
-('Support Manager', 65000, 2)
-('Support Associate', 6500, 2)
-('Marketing Manager', 70000, 3)
-('Marketing Associate', 50000, 3)
-('Development Manager', 100000, 4)
-('Development Associate', 80000, 4 )
+('Sales Manager', 60000, 1),
+('Sales Associate', 40000, 1),
+('Support Manager', 65000, 2),
+('Support Associate', 6500, 2),
+('Marketing Manager', 70000, 3),
+('Marketing Associate', 50000, 3),
+('Development Manager', 100000, 4),
+('Development Associate', 80000, 4);
 
 
 
 INSERT INTO
+first_name, last_name, role_id, manager_id
+
+VALUES
+('Mike', 'Miller', 1, 2),
+('John', 'Winters', 2, NULL),
+('Mike', 'Miller', 3, 4 ),
+('Mike', 'Miller', 4, NULL),
+('Mike', 'Miller', 5, 6),
+('Mike', 'Miller', 6, NULL),
+('Mike', 'Miller', 7, 8),
+('Mike', 'Miller', 8, NULL);
