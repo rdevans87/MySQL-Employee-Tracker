18ca5408642725fd5292ec8f74043c68155456ee
Ryan Evans <rdevans87@gmail.com>
2021-05-27 06:23:04 -0400

    deleted file  connections file and index working with single file structure

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,24 @@
 const connection = require("mysql");
-// const inquirer = require("inquirer");
-const db = require("./db");
-require("console.table");
+const inquirer = require("inquirer");
 
+const mysql = require('mysql')
+// const inquirer = require('inquirer')
+
+const dotenv = require('dotenv').config();
+
+const connection = mysql.createConnection(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+   { 
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+   }
+
+);
+
+connection.connect((err) => {
+    if (err) throw err;
+    start();
+});
